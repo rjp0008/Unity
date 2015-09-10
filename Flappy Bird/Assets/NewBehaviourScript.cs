26c5e8ad895ba41c9ffc2e88c85cc96e494526d8
Robert Perez <robertjasonperez@gmail.com>
2015-09-09 23:07:54 -0500

    Initial Commit
    Work through roll a ball tutorial

---
+++ Flappy Bird/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ RollDaBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        rb.AddForce(movement*speed);
+    }
+}
