a3932d7cd7bb85176d22e12a5bb8729ebc886270
Robert Perez <robertjasonperez@gmail.com>
2015-09-13 19:50:32 -0500

    Rotating asset

--- RollDaBall/Assets/Scripts/PlayerController.cs
+++ RollDaBall/Assets/Scripts/PlayerController.cs
@@ -12,19 +12,14 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
-    {
-
-    }
-
-
     void FixedUpdate()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
         var movement = new Vector3(horizontal, 0, vertical);
-
+        
         rb.AddForce(movement*speed);
+        rb.AddForce(rb.velocity * -0.1f);
     }
 }
