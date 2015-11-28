ed97c7985071e03cf419136fe69ebd4b52911b2e
Robert Perez <robertjasonperez@gmail.com>
2015-11-28 13:45:38 -0600

    Asteroid added
    Added asteroid prefab and explosion visual effects

---
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject Explosion;
+    public GameObject PlayerExplosion;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary") return;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+        var rb = gameObject.GetComponent<Rigidbody>();
+        if (other.tag == "Player")
+        {
+            var otherRb = other.gameObject.GetComponent<Rigidbody>();
+            Instantiate(PlayerExplosion, otherRb.position, otherRb.rotation);
+        }
+        Instantiate(Explosion, rb.position, rb.rotation);
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+
+    public float Tumble;
+
+    void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere*Tumble;
+    }
+}
