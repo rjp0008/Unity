8eab8a547cd327be3268dfc90aecfccc30a3bd49
Robert Perez <robertjasonperez@gmail.com>
2015-11-28 13:54:13 -0600

    GameController created
    New gameobject created to control the game flow and manage spawning
    asteroids

---
+++ SpaceShooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject Hazard;
+    public GameObject Player;
+
+    public Vector3 spawnValues;
+
+    void Start()
+    {
+        SpawnWaves();
+    }
+
+    private void SpawnWaves()
+    {
+        var vec = new Vector3(Random.Range(-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
+        var quat = Quaternion.identity;
+        Instantiate(Hazard, vec, quat);
+    }
+}
