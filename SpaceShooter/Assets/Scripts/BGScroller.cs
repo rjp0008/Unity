3b5e25c78e63d406c2f032f94e815b63c4be609c
Robert Perez <robertjasonperez@gmail.com>
2015-11-30 21:45:53 -0600

    Scrolling background
    More implementation from following along with tutorial video

---
+++ SpaceShooter/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+
+    public float scrollSpeed;
+    public float tileSizedZ;
+
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    startPosition = GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float newPosition = Mathf.Repeat(Time.time * scrollSpeed,tileSizedZ);
+	    var transform = GetComponent<Transform>();
+	    transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
