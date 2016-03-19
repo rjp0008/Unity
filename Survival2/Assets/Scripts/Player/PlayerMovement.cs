20f0aee1ac8607bd0c9090fa62f3ee4a0c24c6b1
Robert Perez <robertjasonperez@gmail.com>
2016-03-19 11:57:46 -0500

    PlayerScript updated

--- Survival2/Assets/Scripts/Player/PlayerMovement.cs
+++ Survival2/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,59 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidBody;
+    private int floorMask;
+    private float camRayLength = 100f;
+
+    public void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h,v);
+        Turning();
+        Animating(h,v);
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    private void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay,out floorHit,camRayLength,floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+
+    private void Animating(float h, float v)
+    {
+        bool walking = h != 0 || v != 0;
+        anim.SetBool("IsWalking",walking);
+    }
 }
