5707b47a914da5b4f51351f5d89dde410346189d
Robert Perez <robertjasonperez@gmail.com>
2016-04-03 15:09:00 -0500

    Scoring added

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -11,15 +11,21 @@ public class BallMovement : MonoBehaviour
     private Rigidbody rb;
     private Transform tf;
 
-    // Use this for initialization
+    private float timeBetweenSpeedups = 5f;
+    private float timeSinceLastSpeedup;
+    private float speedupFactor = 1.1f;
+
+    private GameManager manager;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        var initialVelocity = Random.insideUnitCircle*100;
-        rb.AddForce(new Vector3(initialVelocity.x, 0, initialVelocity.y));
+        var x = Random.Range(0, 2) == 0 ? -1 : 1;
+        var y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector3(x, 0, y);
+        manager = FindObjectOfType<GameManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
@@ -27,15 +33,22 @@ public class BallMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //if (Mathf.Abs(rb.velocity.x) < 1)
-        //{
-        //    var temp = rb.velocity;
-        //    while (Mathf.Abs(temp.x) <1 )
-        //    {
-        //        temp.x *= temp.x*1.1f;
-        //    }
-        //    rb.AddForce(temp);
-        //}
-       }
+        timeSinceLastSpeedup += Time.deltaTime;
+        if (timeSinceLastSpeedup > timeBetweenSpeedups)
+        {
+            timeSinceLastSpeedup = 0;
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z) * speedupFactor;
+        }
+    }
+    
+    
 
-  }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            manager.PlayerScored(rb.transform.position.x);
+            manager.StartRound();
+        }
+    }
+}
---
+++ Pong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Text playerOneScore;
+    public Text playerTwoScore;
+
+    public GameObject ball;
+
+    private GameObject currentBall;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartRound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+
+    public void StartRound()
+    {
+        if (currentBall != null)
+            Destroy(currentBall);
+        currentBall = Instantiate(ball);
+    }
+
+    public void PlayerScored(float ballXPosition)
+    {
+        UpdateScore(ballXPosition > 0 ? playerOneScore : playerTwoScore);
+    }
+
+    private void UpdateScore(Text scoreText)
+    {
+        var currentScoreText = scoreText.text.Split(' ');
+        var currentScore = Convert.ToInt32(currentScoreText[1]);
+        currentScore += 10;
+        scoreText.text = currentScoreText[0] + " " + currentScore;
+    }
+}
