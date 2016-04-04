74821fa3be4ae63a15cc876a0cbbe57327e2da8d
Robert Perez <robertjasonperez@gmail.com>
2016-04-04 18:56:34 -0500

    Space invaders project created
    moving player and shot behavior started

---
+++ SpaceInvaders/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private float speed = 2f;
+	// Use this for initialization
+	void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float input = Input.GetAxis("Horizontal");
+        rb.transform.position = rb.transform.position + new Vector3(-1*input*Time.deltaTime*speed, 0, 0);
+    }
+}
