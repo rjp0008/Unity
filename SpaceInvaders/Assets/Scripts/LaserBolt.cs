9dbe139820e5f70b79bddb8646988ca62e6688f2
Robert Perez <robertjasonperez@gmail.com>
2016-04-04 19:38:53 -0500

    Prefabs created
    Shot and player prefab created

---
+++ SpaceInvaders/Assets/Scripts/LaserBolt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBolt : MonoBehaviour
+{
+    
+    public float boltSpeed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(rb.position.y) > 15)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            rb.transform.position += new Vector3(0,boltSpeed * Time.deltaTime,0);
+        }
+    }
+}
