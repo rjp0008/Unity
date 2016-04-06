05db6021d2c5b7db9d60ab3cd074f74b02e0741c
Robert Perez <robertjasonperez@gmail.com>
2016-04-05 19:18:58 -0500

    enemy update

--- SpaceInvaders/Assets/Scripts/EnemyController.cs
+++ SpaceInvaders/Assets/Scripts/EnemyController.cs
@@ -2,28 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 public class EnemyController : MonoBehaviour
 {
     public GameObject enemy;
     private List<Transform> enemyTransforms = new List<Transform>();
 
+
     private float secondsBeforeMovement = 1;
     private float secondsSinceMovement = 0;
 	// Use this for initialization
 	void Start ()
 	{
-        var enemies = GameObject.FindGameObjectsWithTag("Baddie");
-	    foreach (var enemy in enemies)
-	    {
-	        enemyTransforms.Add(enemy.GetComponent<Transform>());
-	    }
+       NewWave();
     }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update()
+    {
+
+    }
 
     void FixedUpdate()
     {
@@ -35,6 +34,17 @@ public class EnemyController : MonoBehaviour
             {
                 enemyLocation.position += Vector3.right; 
 
+            }
+        }
+    }
+
+    void NewWave()
+    {
+        for (int x = -5; x < 5; x+=2)
+        {
+            for (int y = 10; y > 7; y--)
+            {
+                Instantiate(enemy, new Vector3(x, y, 0),new Quaternion(0,0,90,10));
             }
         }
     }
